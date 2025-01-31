669347cc3a5540a39368b3a0ae27fb32f814c24b
IGYEONGMUN <doehdotn2@naver.com>
2025-01-31 18:39:07 +0900

    250131

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //스크롤내림//스크롤값먼저알아내보자
-window.addEventListener("scroll",()=>{
-    if(window.scrollY>50){
-        document.querySelector(".gnb").classList.add("active");
-    }
-    else{
-        document.querySelector(".gnb").classList.remove("active");
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    document.querySelector(".gnb").classList.add("active");
+    document.querySelector(".banner").classList.add("active");
+  } else {
+    document.querySelector(".gnb").classList.remove("active");
+    document.querySelector(".banner").classList.remove("active");
+  }
+});
